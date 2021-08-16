b3321717dbfd5af2dbd7a2d10fef30cd226e08f1
Haruki <haruki24816@gmail.com>
2021-08-16 14:32:49 +0900

    オプションを変更

--- option_dict.py
+++ option_dict.py
@@ -1,20 +1,24 @@
 OPTION_DICT = {
 
     "mp3(192kbps)": {
+        "retries": 4,
         "format": "bestaudio",
         "postprocessors": [{"key":"FFmpegExtractAudio","preferredcodec":"mp3","preferredquality":"192"}]
     },
 
     "mp3(64kbps)": {
+        "retries": 4,
         "format": "bestaudio",
         "postprocessors": [{"key":"FFmpegExtractAudio","preferredcodec":"mp3","preferredquality":"64"}]
     },
 
     "mp4": {
+        "retries": 4,
         "format": "best[ext=mp4]"
     },
 
     "mp4(最高品質)": {
+        "retries": 4,
         "format": "bestvideo+bestaudio",
         "merge_output_format": "mp4"
     }
